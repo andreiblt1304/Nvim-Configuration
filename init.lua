6ab2f6ab0539551437a92b0e05dbb747bf11202f
Andrei Baltariu <102856257+andreiblt1304@users.noreply.github.com>
2024-01-23 09:15:55 +0200

    Added rust analyzer

--- init.lua
+++ init.lua
@@ -586,7 +586,7 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
