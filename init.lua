2f9644eff2309fd93444acf7416e1c08c474ab29
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-04-19 16:16:29 +0300

    Added inlay hints

--- init.lua
+++ init.lua
@@ -395,6 +395,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(args.buf, true)
+        end
+        -- whatever other lsp config you want
+    end
+})
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
